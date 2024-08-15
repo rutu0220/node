221c4711bce814608d2acca06937062a03b7cf47
Rutuja Athane <109471615+rutu0220@users.noreply.github.com>
2024-08-15 20:01:00 +0530

    Create math.js

---
+++ basics/math.js
@@ -0,0 +1,14 @@
+/*console.log(Math.ceil(4.3))
+console.log(Math.floor(4.3))
+console.log(Math.abs(-5))
+console.log(Math.round(5.6))
+console.log(Math.min(5,4,1))
+console.log(Math.max(10,4,1))*/
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10))
+console.log(Math.floor(Math.random()*10)+1)
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
